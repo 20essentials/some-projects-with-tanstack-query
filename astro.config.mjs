c726548a94ee022ff9ebbfacb7a0cb03448bd4eb
DominicDev <s0l0estudi4r@gmail.com>
2026-09-19 12:35:52 +0200

    v0.0.1

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  build: {
+    assets: 'static'
+  },
+
+  base: import.meta.env.DEV ? undefined : '/project-1020/',
+  site: import.meta.env.DEV
+    ? 'http://localhost:4321/'
+    : 'https://20essentials.github.io/project-1020/'
+});
